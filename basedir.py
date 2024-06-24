7076753703cdb7d0173fd8541046e8944fe32141
Metin Ersin Arıcan <metin.ersin.arican@gmail.com>
2024-06-24 23:56:23 +0300

    database directory

--- basedir.py
+++ basedir.py
@@ -1,4 +1,4 @@
 from typing import Literal
 
 # change this to the path of the KITTI dataset on your machine
-BASEDIR : Literal['/mnt/d/KITTI Dataset']
+BASEDIR : Literal['/mnt/d/KITTI Dataset'] = '/mnt/d/KITTI Dataset'
