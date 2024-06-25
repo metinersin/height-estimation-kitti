3bfbecc90ee95d013b5f0e2c8d3f0d2174bffa5c
Metin Ersin Arıcan <metin.ersin.arican@gmail.com>
2024-06-25 09:19:08 +0300

    load and save calibration matrices

--- projection.py
+++ projection.py
@@ -43,6 +43,11 @@ elif cam_no == "02":
     img = np.asarray(dataset.get_cam2(img_no))
 elif cam_no == "03":
     img = np.asarray(dataset.get_cam3(img_no))
+else:
+    raise ValueError(f'Invalid camera number: {cam_no}')
+
+img_height = img.shape[0]
+img_width = img.shape[1]
 
 # save the image
 pprint(f'Image shape: {img.shape}')
@@ -56,3 +61,32 @@ pprint(f'Velodyne points shape: {points_velo.shape}')
 # save the velodyne points
 with open(os.path.join(outputdir, f'velo_{img_no:06}.pkl'), 'wb') as f:
     pkl.dump(points_velo, f)
+
+# read the calibration matrices
+if cam_no == "00":
+    T_cam_velo : np.ndarray = dataset.calib.T_cam0_velo
+    Rrect      : np.ndarray = dataset.calib.R_rect_00
+    Prect      : np.ndarray = dataset.calib.P_rect_00
+elif cam_no == "01":
+    T_cam_velo = dataset.calib.T_cam1_velo
+    Rrect      = dataset.calib.R_rect_01
+    Prect      = dataset.calib.P_rect_01
+elif cam_no == "02":
+    T_cam_velo = dataset.calib.T_cam2_velo
+    Rrect      = dataset.calib.R_rect_02
+    Prect      = dataset.calib.P_rect_02
+elif cam_no == "03":
+    T_cam_velo = dataset.calib.T_cam3_velo
+    Rrect      = dataset.calib.R_rect_03
+    Prect      = dataset.calib.P_rect_03
+else:
+    raise ValueError(f'Invalid camera number: {cam_no}')
+
+pprint(f'T_cam_velo: {T_cam_velo}')
+pprint(f'Rrect: {Rrect}')
+pprint(f'Prect: {Prect}')
+
+# save the calibration matrices
+np.savetxt(os.path.join(outputdir, f'T_cam_velo_{img_no:06}.txt'), T_cam_velo)
+np.savetxt(os.path.join(outputdir, f'Rrect_{img_no:06}.txt'), Rrect)
+np.savetxt(os.path.join(outputdir, f'Prect_{img_no:06}.txt'), Prect)
